ccc71d23e71e98d498faca68d86d4fea55e3ed68
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-16 19:05:18 +0200

    app<>wrapped app with necessary providers and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-
+import { Route, Routes } from "react-router";
+import Auth from "./pages/Auth";
+import { NextUIProvider } from "@nextui-org/system";
+import Dashboard from "./pages/dashboard/layout";
+import DashboardMain from "./pages/dashboard";
+import ProtectedRoute from "./components/base/ProtectedRoute";
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<NextUIProvider>
+			<Routes>
+				<Route
+					path='/'
+					element={<Auth />}
+				/>
+				<Route
+					path='/dashboard'
+					element={
+						<ProtectedRoute>
+							<Dashboard />
+						</ProtectedRoute>
+					}
+				>
+					<Route
+						index
+						element={<DashboardMain />}
+					/>
+				</Route>
+			</Routes>
+		</NextUIProvider>
+	);
 }
 
-export default App
+export default App;
