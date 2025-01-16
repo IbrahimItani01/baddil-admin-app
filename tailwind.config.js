45ca40452a8615d3b85567d56a6c0ecca2d34b92
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-16 19:03:09 +0200

    configs<>added preset colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
-const {nextui} = require('@nextui-org/theme');
+const { nextui } = require("@nextui-org/theme");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [nextui()],
-}
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#e60000",
+				"dark-primary": "#b30000",
+				"dark-bg": "#140000",
+				"light-bg": "#FFFFFF",
+				"black-font": "#000000",
+				"white-font": "#FFFFFF",
+				"light-gray": "#F7F7F7",
+				"dark-gray": "#333333",
+				"dark-success": "#27ae60",
+				"light-success": "#2ecc71",
+			},
+		},
+	},
+	plugins: [nextui()],
+};
