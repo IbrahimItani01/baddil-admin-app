000ec573a34b8215e787d037b8973fdec84bbf2c
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-17 22:42:56 +0200

    components<>declared dashboard finances component

---
+++ src/pages/dashboard/pages/Finances.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+	forExpenses?: boolean;
+}
+
+const Finances = ({forExpenses = false}:Props) => {
+	return <div></div>;
+};
+
+export default Finances;
