258ff6b14d553e05bca0e7b7de73d696fccddf49
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-17 22:39:23 +0200

    app<>added dashboard related child routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import { NextUIProvider } from "@nextui-org/system";
 import Dashboard from "./pages/dashboard/layout";
 import DashboardMain from "./pages/dashboard";
 import ProtectedRoute from "./components/base/ProtectedRoute";
+import MeetupVerify from "./components/base/MeetupVerify";
+import Users from "./pages/dashboard/pages/Users";
+import Barters from "./pages/dashboard/pages/Barters";
+import Tiers from "./pages/dashboard/pages/Tiers";
+import Finances from "./pages/dashboard/pages/Finances";
+import Settings from "./pages/dashboard/pages/Settings";
 function App() {
 	return (
 		<NextUIProvider>
@@ -24,7 +30,43 @@ function App() {
 						index
 						element={<DashboardMain />}
 					/>
+					<Route
+						path='barterers'
+						element={<Users />}
+					/>
+					<Route
+						path='brokers'
+						element={<Users isBroker={true} />}
+					/>
+					<Route
+						path='barters'
+						element={<Barters />}
+					/>
+					<Route
+						path='barters-locations'
+						element={<Barters withLocations={true} />}
+					/>
+					<Route
+						path='tiers'
+						element={<Tiers />}
+					/>
+					<Route
+						path='revenues'
+						element={<Finances />}
+					/>
+					<Route
+						path='expenses'
+						element={<Finances forExpenses={true} />}
+					/>
+					<Route
+						path='settings'
+						element={<Settings />}
+					/>
 				</Route>
+				<Route
+					path='/meetup-verify/:email?'
+					element={<MeetupVerify />}
+				/>
 			</Routes>
 		</NextUIProvider>
 	);
