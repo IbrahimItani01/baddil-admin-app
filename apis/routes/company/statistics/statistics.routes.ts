6f6b61550eb375ef01d4588b232f3df948b7ade1
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-17 22:38:12 +0200

    routes<>handled fetching statistics apis

---
+++ apis/routes/company/statistics/statistics.routes.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { APIS_BASE_URL } from "../../../main"; 
+
+const STATISTICS_ENDPOINT = `${APIS_BASE_URL}/statistics/user-count`;
+
+export const fetchUserCounts = async () => {
+  try {
+    const token = localStorage.getItem("authToken"); 
+    const response = await axios.get(STATISTICS_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${token}`, 
+      },
+    });
+    return response.data; 
+  } catch (error) {
+    console.error("Error fetching user counts:", error.response?.data || error.message);
+    throw error; 
+  }
+};
