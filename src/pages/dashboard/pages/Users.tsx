d5bc5c3544a904f56fcff3c713de26a9885f5d87
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-17 22:43:23 +0200

    components<>declared dashboard users component

---
+++ src/pages/dashboard/pages/Users.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    isBroker?: boolean
+}
+
+const Users = ({isBroker = false}:Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Users
