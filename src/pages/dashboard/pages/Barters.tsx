8628bcd7e91a4e12bafad330ea0536fd1f47b879
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-17 22:42:47 +0200

    components<>declared dashboard barters component

---
+++ src/pages/dashboard/pages/Barters.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+ 
+interface Props {
+    withLocations?: boolean
+}
+const Barters = ({withLocations = false}:Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+ 
+
+
+export default Barters
