8309dea6c13d0f11315c9d00d623c2cdfeaf2c9a
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-20 01:10:14 +0200

    store<>cleaned code

--- store/store.ts
+++ store/store.ts
@@ -3,7 +3,6 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 import authSlice from "./slices/auth.slice";
 import userSlice from "./slices/user.slice";
-import systemSlice from "./slices/system.slice";
 import bartersSlice from "./slices/barters.slice";
 import usersSlice from "./slices/users.slice";
 import profitsSlice from "./slices/profits.slice";
@@ -18,7 +17,6 @@ const store = configureStore({
 	reducer: {
 		user: userSlice,
 		auth: authSlice,
-		system: systemSlice,
 		barters: bartersSlice,
 		users: usersSlice,
 		expenses: expensesSlice,
