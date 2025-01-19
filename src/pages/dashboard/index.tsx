243769dd55adfa6af0b346759fc4c0729bea7431
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-19 14:18:24 +0200

    index<>handled dashboard main overview screen

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,15 @@
-import { Search } from "@nextui-org/shared-icons";
+import BartersOverview from "../../components/base/dashboard/overview/BartersOverview";
+import LocationsOverview from "../../components/base/dashboard/overview/LocationsOverview";
+import UsersOverview from "../../components/base/dashboard/overview/UsersOverview";
 
 const DashboardMain = () => {
 	return (
-        <>
-            <div>
-                <Search/>
-            </div>
-        </>
-    );
+		<div className="flex flex-wrap gap-5 mx-5">
+			<UsersOverview />
+			<BartersOverview />
+			<LocationsOverview />
+		</div>
+	);
 };
 
 export default DashboardMain;
